3366691231e567335c71bfd941d306831537d430
leost28 <leost28@gmail.com>
2021-12-07 18:02:10 +0100

    Formulario registro creado

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  formulario: FormGroup;
+
+  constructor() {
+    this.formulario = new FormGroup({
+      first_name: new FormControl('', [Validators.required]),
+      last_name: new FormControl('', [Validators.required]),
+      username: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required]),
+      image: new FormControl(''),
+    })
+   }
 
   ngOnInit(): void {
+  }
+
+  
+  onSubmit() {
+    console.log('envío de registro');
+  
   }
 
 }
